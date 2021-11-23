bd5d21b16b88205c57d33af136150e60f39b44d6
christino <christino@wylog.com>
2021-11-23 13:20:11 +0300

    node and python always

--- index.js
+++ index.js
@@ -1,94 +1,40 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const PORT = process.env.PORT || 5000;
-const firebaseApp = require("./src/firebase");
-const {
-  collection,
-  getFirestore,
-  getDocs,
-  addDoc,
-} = require("firebase/firestore/lite");
-const linkPreviewGenerator = require("link-preview-generator");
-const puppeteer = require("puppeteer");
-// MIDDLEWARES
+const { resolve } = require("path");
+const fs = require("fs");
+const { spawn } = require("child_process");
+const os = require("os");
+const port = process.env.PORT || 3000;
+const JSON5 = require("json5");
 
-db = getFirestore(firebaseApp);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app
-  .use(express.static(path.join(__dirname, "public")))
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .set("views", path.join(__dirname, "views"))
-  .set("view engine", "ejs");
+app.post("/api/preview", (req, res) => {
+  const pythoncmd = os.type().toLowerCase() === "darwin" ? "python3" : "python";
+  console.log(req.body);
+  python = spawn(pythoncmd, [
+    "-u",
+    resolve(__dirname, "scripts", "main.py"),
+    req.body.url,
+  ]);
 
-app.get("/api/articles", (req, res) => {
-  getArticles(db)
-    .then((articles) => {
-      res.send(articles);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+  python.stdout.on("data", function (data) {
+    try {
+      res.send(JSON5.parse(data.toString("binary")));
+    } catch (error) {
+      res.send(error);
+    }
+  });
 
-app.post("/api/article", (req, res) => {
-  getPreviewData(req.body.url).then((data) => {
-    addArticle(db, { ...data, url: req.body.url }).then((article) => {
-      res.send(article);
-    });
+  // in close event we are sure that stream from child process is closed
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
   });
 });
 
-app.get("/api/pdf", (req, res) => {
-  printPDF(
-    "https://programmingwithswift.com/how-to-save-a-file-locally-with-flutter/"
-  ).then((pdf) => {
-    res.set({
-      "Content-Type": "application/pdf",
-      "Content-Length": pdf.length,
-    });
-    res.send(pdf);
-  });
-});
+app.post("/api/download", (req, res) => {});
 
-app.post("/api/getinfo", (req, res) => {
-  getPreviewData(req.body.url).then((data) => {
-    res.send(data);
-  });
-});
-
-async function getArticles(db) {
-  const articleCols = collection(db, "articles");
-  const snapshot = await getDocs(articleCols);
-  return snapshot.docs.map((doc) => doc.data());
-}
-
-async function addArticle(db, body) {
-  const article = collection(db, "articles");
-  return await addDoc(article, body);
-}
-
-async function getPreviewData(url) {
-  return linkPreviewGenerator(url);
-}
-
-async function printPDF(url) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "networkidle0",
-  });
-  const pdf = await page.pdf({
-    format: "A4",
-  });
-
-  await browser.close();
-  return pdf;
-}
-
-app.get("/", (req, res) => res.render("pages/index"));
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- scripts/main.py
+++ scripts/main.py
@@ -1,36 +1,34 @@
 from linkpreview import link_preview
 import pdfkit
 import sys
-import json
-from time import sleep
-
-Instance = None
 
 
-def main():
+def preview_link(link: str):
+    try:
+        preview = link_preview(link)
+        print({
+            'title': preview.title,
+            'description': preview.description
+        })
+    except Exception as e:
+        print({"message": f"Something went wrong with the link preview. {e}"})
+
+
+def download(url):
     try:
         pdfkit.from_url(
-            'https://stacksecrets.com/flutter/flutter-textfield-decoration-in-depth#Customizing_The_Look_And_Feel_Of_TextField', 'shaurya.pdf')
+            'https://stacksecrets.com/flutter/flutter-textfield-decoration-in-depth#Customizing_The_Look_And_Feel_Of_TextField', './articles/.pdf')
         preview_link(
             "https://stacksecrets.com/flutter/flutter-textfield-decoration-in-depth#Customizing_The_Look_And_Feel_Of_TextField")
-        print({"message": "Starting browser"})
 
     except Exception as e:
         print({"message": f"Something went wrong with the browser. {e}"})
         sleep(1)
 
 
-def preview_link(link):
-    try:
-        preview = link_preview(link)
-        print("title:", preview.title)
-        print("description:", preview.description)
-        print("image:", preview.image)
-        print("force_title:", preview.force_title)
-        print("absolute_image:", preview.absolute_image)
-    except Exception as e:
-        print({"message": f"Something went wrong with the link preview. {e}"})
+# if sys.argv[1] == "preview":
+#     preview_link(sys.argv[2])
+# elif sys.argv[1] == "download":
+#     download()
 
-
-if __name__ == "__main__":
-    main()
+preview_link(sys.argv[1])
