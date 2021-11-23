feffafb00e91ee79ec13dd6a52b1b26e5769fe79
christino <christino@wylog.com>
2021-11-23 12:09:19 +0300

    changed chromedriver behaviour

--- scripts/main.py
+++ scripts/main.py
@@ -1,39 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdrivermanager.chrome import ChromeDriverManager
 from linkpreview import link_preview
-
+import pdfkit
 import sys
 import json
 from time import sleep
-
-# SET OPTIONS
-chrome_options = Options()
-chrome_options.add_argument("--lang=en")
-chrome_options.add_argument("--headless")
 
 Instance = None
 
 
 def main():
-
-    global Instance
-
     try:
-        Instance = webdriver.Chrome(
-            ChromeDriverManager().download_and_install()[0], options=chrome_options)
-
+        pdfkit.from_url(
+            'https://stacksecrets.com/flutter/flutter-textfield-decoration-in-depth#Customizing_The_Look_And_Feel_Of_TextField', 'shaurya.pdf')
         preview_link(
             "https://stacksecrets.com/flutter/flutter-textfield-decoration-in-depth#Customizing_The_Look_And_Feel_Of_TextField")
-
-        # Instance.execute_script('window.print();')
-
         print({"message": "Starting browser"})
 
     except Exception as e:
         print({"message": f"Something went wrong with the browser. {e}"})
         sleep(1)
-        Instance.quit()
 
 
 def preview_link(link):
