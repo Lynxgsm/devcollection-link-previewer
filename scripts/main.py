a1686080f7ca85e8060198cd99c67ec680fd5271
christino <christino@wylog.com>
2021-11-23 11:56:40 +0300

    using selenium

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from linkpreview import link_preview
 
 import sys
 import json
@@ -23,10 +24,10 @@ def main():
         Instance = webdriver.Chrome(
             ChromeDriverManager().install(), options=chrome_options)
 
-        Instance.get(
+        preview_link(
             "https://stacksecrets.com/flutter/flutter-textfield-decoration-in-depth#Customizing_The_Look_And_Feel_Of_TextField")
 
-        Instance.execute_script('window.print();')
+        # Instance.execute_script('window.print();')
 
         print({"message": "Starting browser"})
 
@@ -34,6 +35,18 @@ def main():
         print({"message": f"Something went wrong with the browser. {e}"})
         sleep(1)
         Instance.quit()
+
+
+def preview_link(link):
+    try:
+        preview = link_preview(link)
+        print("title:", preview.title)
+        print("description:", preview.description)
+        print("image:", preview.image)
+        print("force_title:", preview.force_title)
+        print("absolute_image:", preview.absolute_image)
+    except Exception as e:
+        print({"message": f"Something went wrong with the link preview. {e}"})
 
 
 if __name__ == "__main__":
